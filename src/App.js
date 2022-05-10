b838f4ea23a0770322df4fd5061bf64189a58df5
paulotiba <tibapaulo@outlook.com>
2022-05-10 11:15:41 -0300

    estilos e logica do game

--- src/App.js
+++ src/App.js
@@ -22,12 +22,78 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
-  console.log(words)
+
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+
+
+  const pickWordAndCategory = () => {
+    // pegar categotia aleatória
+    const categories = Object.keys(words)
+    const category = categories[Math.floor( Math.random() * Object.keys(categories).length)]
+
+    console.log(category)
+
+    // pegar palavra aleatória
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    console.log(word)
+
+    return {word, category}
+
+
+
+  }
+
+
+// start do jogo
+  const startGame = () => {
+    // pick word and pick category
+    const { word, category } =  pickWordAndCategory()
+    // separar em letras as palavras
+    let wordLetters = word.split("")
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    console.log(word, category)
+    console.log(wordLetters)
+
+
+    // setar os estados
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
+
+
+    setGameStage(stages[1].name)
+  }
+
+
+  // vereficar letter 
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+
+  }
+
+  // restarst do jogo reinicia o jogo
+
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
+
+  
+
+
+
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'game' && <Game />}
-      {gameStage === 'end' && <GameOver />}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'end' && <GameOver  retry={retry}/>}
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,35 @@
 import './Game.css'
 
-const Game = () => {
+const Game = ({verifyLetter}) => {
   return (
-    <div>Game</div>
+    <div className='game'>
+      <p className="points">
+        <span>Pontuação: 000</span>
+        <p>Você tem ...tentativas!</p>
+      </p>
+      <h1>Advinhe a Palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className='letter'>A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name='letter' maxLength={1} required />
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
+
+
+    </div>
   )
 }
 
